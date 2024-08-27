f86526d36febe2a678bacb426a6ae3cdc6604941
faatihat <tomfaah@gmail.com>
2024-08-27 08:02:28 +0100

    adjust production err

--- src/ui/LinkButton.jsx
+++ src/ui/LinkButton.jsx
@@ -9,7 +9,7 @@ function LinkButton({ children, to }) {
   return (
     <Link
       to={to}
-      className="text-sm text-blue-500 hover:text-blue-600 hover:underline"
+      className="n text-sm text-blue-500 hover:text-blue-600 hover:underline "
     >
       {children}
     </Link>
